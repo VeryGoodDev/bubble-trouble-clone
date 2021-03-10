d855f4ac9118278d86791475c413533b1d2f3740
VeryGoodDev <very_good_dev@outlook.com>
2021-03-10 04:11:33 -0700

    Added drawing of spikes on ceiling

--- assets/js/layers/CeilingFloor.js
+++ assets/js/layers/CeilingFloor.js
@@ -4,6 +4,9 @@ import Layer from './Layer.js'
 /**
  * @typedef { import('../types').LevelSpec } LevelSpec
  */
+
+const SPIKE_COUNT = 115
+
 class Ceiling extends Layer {
   constructor(ceilingHeight, ceilingVelocity, width, height) {
     super(width, height)
@@ -31,10 +34,10 @@ class Ceiling extends Layer {
     }
   }
   drawCeiling() {
-    if (this.ceilingHeight > 0) {
-      this.context.fillStyle = `hsl(120, 0%, 60%)`
-      this.context.fillRect(0, 0, this.width, this.ceilingHeight)
-    }
+    // if (this.ceilingHeight > 0) {
+    this.context.fillStyle = `hsl(120, 0%, 60%)`
+    this.context.fillRect(0, 0, this.width, this.ceilingHeight)
+    // }
     this.context.drawImage(this.spikes, 0, Math.max(0, this.ceilingHeight))
   }
 }
@@ -55,7 +58,7 @@ export default class CeilingFloorLayer extends Layer {
    */
   constructor(levelSpec, width, height) {
     super(width, height)
-    const { ceilingHeight = 10, ceilingVelocity = 0, floorHeight = 50 } = levelSpec
+    const { ceilingHeight = 0, ceilingVelocity = 0, floorHeight = 0 } = levelSpec
     this.sublayers = new Composer([
       new Floor(floorHeight, width, height),
       new Ceiling(ceilingHeight, ceilingVelocity, width, height),
@@ -77,7 +80,21 @@ export default class CeilingFloorLayer extends Layer {
  */
 // TODO: 115 spikes
 function getSpikes(width) {
+  const spikeWidth = width / SPIKE_COUNT
+  const spikeHeight = Math.sqrt(spikeWidth ** 2 - (spikeWidth / 2) ** 2)
   const canvas = document.createElement(`canvas`)
+  canvas.width = width
+  canvas.height = spikeHeight
   const context = canvas.getContext(`2d`)
+  context.fillStyle = `hsl(120, 10%, 60%)`
+  let x = 0
+  for (let i = 0; i < SPIKE_COUNT; ++i) {
+    context.beginPath()
+    context.moveTo(x, 0)
+    context.lineTo(x + spikeWidth / 2, spikeHeight)
+    context.lineTo(x + spikeWidth, 0)
+    context.fill()
+    x += spikeWidth
+  }
   return canvas
 }
