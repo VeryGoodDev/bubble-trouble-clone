d9a357ba1dd97582f1f0e772db9b3056620f4f62
VeryGoodDev <very_good_dev@outlook.com>
2021-03-13 04:58:51 -0700

    Couple notes and like one line of code
    Got distracted lol

--- assets/js/layers/WallLayer.js
+++ assets/js/layers/WallLayer.js
@@ -11,13 +11,22 @@ class Wall {
    * @param {WallSpec} wallSpec Specifications for the wall
    */
   constructor(wallSpec) {
+    // Separate properties for open and unlocked, because once
+    // unlocked, it will be in the process of opening for a few
+    // frames, so we need to know the difference
     this.isOpen = false
+    this.isUnlocked = false
     this.type = wallSpec.type
     this.width = WALL_WIDTH
     this.position = wallSpec.position
     this.unlockCondition = wallSpec.unlockCondition
   }
   checkUnlockCondition() {}
+  draw() {}
+  update() {
+    // Nothing to update when it's open
+    if (this.isOpen) return
+  }
 }
 
 export default class WallLayer extends Layer {
