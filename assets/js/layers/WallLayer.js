e0c99e5f75a02fc1116bd2122d2942fb8ba74939
VeryGoodDev <very_good_dev@outlook.com>
2021-03-14 04:16:45 -0600

    Add wall layer
    Supports timed opening, nothing else yet

--- assets/js/layers/WallLayer.js
+++ assets/js/layers/WallLayer.js
@@ -4,6 +4,7 @@ import Layer from './Layer.js'
  * @typedef { import('../types').WallSpec } WallSpec
  */
 
+const WALL_OPENING_VELOCITY = 44
 const WALL_WIDTH = 1920 / 17
 
 class Wall {
@@ -16,18 +17,70 @@ class Wall {
     // frames, so we need to know the difference
     this.isOpen = false
     this.isUnlocked = false
-    this.type = wallSpec.type
     this.width = WALL_WIDTH
     this.position = wallSpec.position
     this.unlockCondition = wallSpec.unlockCondition
+    if (this.unlockCondition.type === `time`) {
+      this.timeUntilUnlock = this.unlockCondition.time
+    }
   }
-  checkUnlockCondition() {}
-  draw() {}
-  update() {
-    // Nothing to update when it's open
+  checkUnlockCondition(time) {
+    if (this.unlockCondition.type === `time`) {
+      if (time >= this.unlockCondition.time) {
+        this.isUnlocked = true
+      }
+    } else if (this.unlockCondition.type === `bubble`) {
+      // TODO:
+    }
+  }
+  /**
+   * @param {CanvasRenderingContext2D} context Context to draw on
+   */
+  // eslint-disable-next-line no-unused-vars
+  draw(context) {}
+  update() {}
+}
+class RemovableWall extends Wall {
+  /**
+   * @param {WallSpec} wallSpec Specifications for the wall
+   * @param {number} wallHeight Total height of the wall
+   */
+  constructor(wallSpec, wallHeight) {
+    super(wallSpec)
+    this.sectionHeight = wallHeight / 2
+  }
+  /**
+   * @param {CanvasRenderingContext2D} context Context to draw on
+   */
+  draw(context) {
     if (this.isOpen) return
+    context.fillStyle = `orange`
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height)
+    // Draw top
+    this.drawWallSection(context, 0)
+    // Draw bottom
+    this.drawWallSection(context, context.canvas.height - this.sectionHeight)
+  }
+  /**
+   * @param {CanvasRenderingContext2D} context Context to draw on
+   */
+  drawWallSection(context, startY) {
+    context.fillRect(this.position, startY, this.width, this.sectionHeight)
+  }
+  /**
+   * @param {CanvasRenderingContext2D} context Context to draw on
+   */
+  update(time) {
+    if (this.isOpen) return
+    if (this.isUnlocked) {
+      if (this.sectionHeight < 0) this.isOpen = true
+      this.sectionHeight -= WALL_OPENING_VELOCITY
+    } else {
+      this.checkUnlockCondition(time)
+    }
   }
 }
+class WithDoorWall extends Wall {}
 
 export default class WallLayer extends Layer {
   /**
@@ -37,6 +90,52 @@ export default class WallLayer extends Layer {
    */
   constructor(walls, width, height) {
     super(width, height)
-    this.walls = walls.map(wallSpec => new Wall(wallSpec))
+    this.walls = walls.map(wallSpec => {
+      if (wallSpec.type === `removable`) {
+        return new RemovableWall(wallSpec)
+      }
+      if (wallSpec.type === `withdoor`) {
+        return new WithDoorWall(wallSpec)
+      }
+      console.error(`Invalid wall type [${wallSpec.type}] provided`)
+      return ``
+    })
+    // FIXME: Testing code, remove
+    if (!this.walls.length) {
+      this.walls.push(
+        new RemovableWall(
+          {
+            position: width / 10,
+            unlockCondition: {
+              type: `time`,
+              time: 5e3,
+            },
+          },
+          height
+        )
+      )
+    }
+    if (this.walls.length) {
+      this.walls.forEach(wall => {
+        wall.draw(this.context)
+      })
+    }
+  }
+  /**
+   * @param {CanvasRenderingContext2D} context Context to draw on
+   */
+  draw(context) {
+    this.walls.forEach(wall => {
+      if (wall.isOpen) return
+      if (wall.isUnlocked) {
+        wall.draw(this.context)
+      }
+    })
+    super.draw(context)
+  }
+  update(time) {
+    this.walls.forEach(wall => {
+      wall.update(time)
+    })
   }
 }
